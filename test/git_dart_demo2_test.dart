68b55ae3c89e239e1d4c5553067bf73e6d731962
minthwaykhant711 <6631501176@lamduan.mfu.ac.th>
2025-08-24 22:53:54 +0700

    added a new feature total paid

--- test/git_dart_demo2_test.dart
+++ test/git_dart_demo2_test.dart
@@ -1,8 +1,35 @@
-import 'package:git_dart_demo2/git_dart_demo2.dart';
-import 'package:test/test.dart';
+import 'dart:io';
+
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
+  List expenses = [
+    {"id": 1, "item": "lunch", "paid": 70},
+    {"id": 2, "item": "coffee", "paid": 45},
+    {"id": 3, "item": "room rent", "paid": 1600},
+  ];
+
+
+  print('===== Expense tracking app ===== ');
+  print('1. Show all expenses');
+  print('2. Show total paid');
+  stdout.write('Enter 1 or 2...');
+  // get user input
+  int? choice = int.tryParse(stdin.readLineSync()!);
+  if (choice == null) {
+    print('Invalid input');
+  } else {
+    switch (choice) {
+      case 1:
+        break;
+      case 2:
+        int total = 0;
+        for (var expense in expenses) {
+          total += expense['paid'] as int;
+        }
+        print('Total paid: $total');
+        break;
+      default:
+        print('Invalid input');
+    }
+  }
 }
