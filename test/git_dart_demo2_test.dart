1abc28ce4736d011ee9ae91c3faa11720dda3f5c
Zaw Linn Phyo <6631501194@lamduan.mfu.ac.th>
2025-08-24 22:03:37 +0700

    juinor commit 1

--- test/git_dart_demo2_test.dart
+++ test/git_dart_demo2_test.dart
@@ -1,8 +1,34 @@
-import 'package:git_dart_demo2/git_dart_demo2.dart';
-import 'package:test/test.dart';
+import 'dart:io';
+
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
+  List expenses = [
+    {"id": 1, "item": "lunch", "paid": 70},
+    {"id": 2, "item": "coffee", "paid": 45},
+    {"id": 3, "item": "room rent", "paid": 1600},
+  ];
+
+
+  print('===== Expense tracking app ===== ');
+  print('1. Show all expenses');
+  print('2. Show total paid');
+  stdout.write('Enter 1 or 2...');
+  // get user input
+  int? choice = int.tryParse(stdin.readLineSync()!);
+  if (choice == null) {
+    print('Invalid input');
+  } else {
+    switch (choice) {
+      case 1:
+        // loop to show all expenses
+        for (Map expense in expenses) {
+          print('${expense["id"]} - ${expense["item"]} - ${expense["paid"]}');
+        }
+        break;
+      case 2:
+        break;
+      default:
+        print('Invalid input');
+    }
+  }
 }
